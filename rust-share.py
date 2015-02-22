626d63f8ccf2f92bab7492f824e4037977fa6fdf
Ben Anderson <gravityscore@gmail.com>
2015-02-22 13:48:08 +0800

    Fix code formatting

--- rust-share.py
+++ rust-share.py
@@ -1,4 +1,7 @@
+
 import sublime, sublime_plugin
+import json
+
 ST3 = int(sublime.version()) >= 3000
 if ST3:
 	from urllib.parse import quote_plus
@@ -7,9 +10,9 @@ else:
 	from urllib import quote_plus
 	from urllib2 import urlopen
 
-import json
 
 class RustShareCommand(sublime_plugin.TextCommand):
+
 	def run(self, edit):
 		everything = self.view.substr(sublime.Region(0, self.view.size()))
 		url = "https://play.rust-lang.org/?code=" + quote_plus(everything)
@@ -17,12 +20,13 @@ class RustShareCommand(sublime_plugin.TextCommand):
 		contents = urlopen(shortURL).read()
 		shortened = json.loads(contents.decode("utf-8"))["shorturl"]
 		sublime.set_clipboard(shortened)
+
 		if ST3:
 			self.displayOutput(shortened)
 
-	def displayOutput(self,link):
-	    msg="Your Rust code has been shared online at {0}. \n\nThis link has been copied to your clipboard.\n\nPress Esc to Exit".format(link)
-	    window=sublime.active_window()
+	def displayOutput(self, link):
+	    msg = "Your Rust code has been shared online at:\n\n{0}\n\nThis link has been copied to your clipboard.\nPress escape to close this window.".format(link)
+	    window = sublime.active_window()
 	    self.output_view = window.get_output_panel("textarea")
 	    window.run_command("show_panel", {"panel": "output.textarea"})
 	    self.output_view.set_read_only(False)
